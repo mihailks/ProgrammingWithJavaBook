da74dae459ebd29e53dc4e78b2094f7ec17014a6
Mihail Slavov <inj.slavov@gmail.com>
2022-12-16 16:04:14 +0200

    -

---
+++ DataTypes/P01.java
@@ -0,0 +1,14 @@
+package DataTypes;
+
+import java.util.Scanner;
+
+public class P01 {
+    public static void main(String[] args) {
+        // Scanner scanner = new Scanner(System.in);
+        int a = 52130;
+        byte b = -115;
+        long c = 4825932L;
+        byte d = 97;
+        int e = -10000;
+    }
+}
---
+++ DataTypes/P02.java
@@ -0,0 +1,10 @@
+package DataTypes;
+
+public class P02 {
+    public static void main(String[] args) {
+        double a = 34.567839023;
+        float b = 12.345f;
+        double c = 8923.1234857;
+        float d = 3456.091F;
+    }
+}
